db7b0aafffd03672f3d8a5c634adca050471f47a
clint-gitahi <clingitahi@gmail.com>
2017-05-31 12:57:25 +0300

    adding provider and store

--- src/routes.js
+++ src/routes.js
@@ -5,18 +5,25 @@ import {
   browserHistory,
   IndexRoute
 } from 'react-router';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 
 import { Home, About, Karibu } from './components';
 import { AddImage } from './containers';
 
+const store = configureStore();
+
 const Routes = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Home}>
-      <IndexRoute component={Karibu} />
-      <Route path="/about" component={About} />
-    </Route>
-    <Route path="/new" component={AddImage}></Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={Home}>
+        <IndexRoute component={Karibu} />
+        <Route path="/about" component={About} />
+      </Route>
+      <Route path="/new" component={AddImage}></Route>
+    </Router>
+  </Provider>
+
 )
 
 export default Routes;
