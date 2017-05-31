76c6790c613a753399f61ac47d04bc9a11fdd586
clint-gitahi <clingitahi@gmail.com>
2017-05-31 12:54:27 +0300

    fetching photos actions

--- src/actions/images.js
+++ src/actions/images.js
@@ -2,6 +2,7 @@ import {
   GET_IMAGES,
   GET_IMAGES_SUCCESS,
   GET_IMAGES_FAILURE,
+  SET_SEARCH_BAR
 } from '../constants/images';
 
 // dispatch this function in the ImageContainer.
@@ -11,12 +12,32 @@ function getImages() {
   };
 }
 
+// this is after fetching from the server, this action is intercepted by the reducer and the images are added to the state.
+function getImagesSuccess(images) {
+  return {
+    type: GET_IMAGES_SUCCESS,
+    images
+  };
+}
 
+function getImagesFailure(err) {
+  return {
+    type: GET_IMAGES_FAILURE,
+    err
+  }
+}
 
-
-
-
+// this action creator comes with words, typed by the user.
+function setSearchBar(words) {
+  return {
+    type: SET_SEARCH_BAR,
+    words
+  }
+}
 
 export {
   getImages,
+  getImagesSuccess,
+  getImagesFailure,
+  setSearchBar
 };
